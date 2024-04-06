efe6e5358c88c7ce64f4eed14e6e327ab3ec9178
mstiles-grs <mstiles@grs-usa.com>
2024-04-06 11:25:10 -0500

    WIP: setup

---
+++ playbook/sql_database/startup_commands.go
@@ -0,0 +1,29 @@
+package sqlDB
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func AppStartUp() *sql.DB {
+	db, err := sql.Open("sqlite3", "./mydatabase.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS userinfo (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT,
+		age INTEGER
+	);`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return nil
+	}
+
+	return db
+}
